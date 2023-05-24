13ec02c2041033bc69574f7d3dd294ddbc01a935
FaccioAle5805 <faccioalessandro2005@gmail.com>
2023-05-24 13:11:54 +0200

    Aggiunta modal illustrativo nome e descrizione

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var map;
 var citta = [
-    "Roma",
-    "Torino",
+    {nome:"Roma", desc: "Roma è il capoluogo del Lazio ma anche la capitale d'Italia, composta da 2.863.843 abitanti, fondata il 21 aprile del 753 a.C., su sette colli. Ancora oggi si possono vedere nel suo centro storico. Roma, è stata da millenni capitale del Impero Romano e si estendeva su tutto il bacino del Mar Mediterraneo."}
+    /*"Torino",
     "Milano",
     "Napoli",
     "Bari",
@@ -9,11 +9,21 @@ var citta = [
     "Bologna",
     "Trento",
     "Perugia",
-    "Piacenza"
-]
+    "Piacenza"*/
+];
+
+let div = `
+    <div id="alert">
+        <div>
+            <h3 id="titolo"></h3>
+        </div>
+        <div>
+            <p id = "descrizione"></p>
+        </div>
+    </div>`
 
 window.onload = async function(){
-    let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=" + citta[0]);
+    let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=" + citta[0].nome);
     let vet = await busta.json();
     let coord = [parseFloat(vet[0].lon), parseFloat(vet[0].lat)];
 
@@ -28,14 +38,13 @@ window.onload = async function(){
         view:new ol.View({
             /* Array di Float: coordinaete (lon.lat) */
             center: ol.proj.fromLonLat(coord),
-            zoom: 5
+            zoom: 5.5
         })
     });
 
-        
     for(var i = 0; i < citta.length; i++){
-            
-        busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=" + citta[i]);
+
+        busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=" + citta[i].nome);
         vet = await busta.json();
         coord = [parseFloat(vet[0].lon), parseFloat(vet[0].lat)];
         //Incercettazione del click sulla mappa
@@ -51,9 +60,25 @@ window.onload = async function(){
         /** forEachFeatureAtPixel: passo da avere i pixel premuti ad avere un marker
          * secondo parametro --> funzione richiamata per ogni feature trovata
          */
-        let marker = map.forEachFeatureAtPixel(evento.pixel, feature => feature) // (feature) =>{return feature;});
-        if(marker)
-            alert(marker.name);
+        let marker = map.forEachFeatureAtPixel(evento.pixel, feature => feature); // (feature) =>{return feature;});
+        alert(marker.dati.nome + " - " + marker.dati.desc);
+        console.log(marker.dati);
+
+        let body = document.getElementsByTagName("main")[0];
+        var modal = document.getElementById("myModal");
+
+        var span = document.getElementsByClassName("close")[0];
+
+        document.getElementById("titolo").innerHTML = marker.dati.nome;
+        document.getElementById("desc").innerHTML = marker.dati.desc;
+
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
     });
 }
 
@@ -82,11 +107,15 @@ function aggiungiLayer(mappa, pathImg){
  * @param {*} lon:float Longitudine
  * @param {*} lat:float Latitudine
  */
-function aggiungiMarker(layer ,nome, lon, lat){
+function aggiungiMarker(layer, comune, lon, lat){
     let marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([lon, lat])));
-    
-    marker.name = nome;
-
+    marker.dati = {
+        nome:comune.nome,
+        lon:lon,
+        lat:lat,
+        desc:comune.desc
+    };
+    marker.name = comune.nome;
     //Inserisce il marker nel layer passato nel parametro
     layer.getSource().addFeature(marker);
 }
